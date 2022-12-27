01019311826b8bc488fad4f0fdabf931684764c6
nninyeong <minyk0512@gmail.com>
2022-12-27 20:36:10 +0900

    플레이어 이동 구현

--- KimMinYeong/Sokoban/Sokoban/Program.cs
+++ KimMinYeong/Sokoban/Sokoban/Program.cs
@@ -19,3 +19,88 @@ Console.Clear();  // 출력된 모든 내용을 지운다.
 //ConsoleKeyInfo keyInfo = Console.ReadKey();  // 현재 눌려진 키에 대한 여러 정보가 들어있음k
 //Console.WriteLine(keyInfo.Key);  // 눌려진 키가 무엇인지
 
+int playerX = 0;
+int playerY = 0;
+
+int boxX = 5;
+int boxY = 3;
+
+// 15 * 10 맵 크기를 제한할 것임 (폰트때문에 가로가 좀 더 넓어야 예쁨)
+
+
+// 게임 루프 == 프레임(Frame)
+while (true)
+{
+    Console.Clear();  // 이전 프레임을 지운다. 게임루프 한 번이 한 프레임이기 때문에 이전 프레임의 내용을 지워주면 실시간으로 이동하는 것처럼 보인다(이전 프레임 지우고 입력이 있으면 그만큼 옆으로)
+    // -------------------------------- Render ---------------------------------
+    // 플레이어 출력하기
+    Console.SetCursorPosition(playerX, playerY);
+    Console.Write("P");
+
+    Console.SetCursorPosition(boxX, boxY);
+    Console.Write("O");
+
+    // -------------------------------- ProcessInput ------------------------------
+    ConsoleKey key = Console.ReadKey().Key;  // Console.ReadyKey()를 하면 나오는 결과가 ConsoleKeyInfo 타입이라는 것을 알 수 있다. 그렇기 때문에 바로 .Key를 붙여서 ConsoleKey 타입이 나오고, 사용할 수 있는 것
+    // ConsoleKey와 ConsoleKeyInfo는 서로 다른 타입이다. ConsoleKeyInfo에 ConsoleKey 타입의 정보가 포함된다.따라서 프로퍼티를 통해 접근하여 가져올 수 있다.
+
+    
+
+
+
+    // -------------------------------- Update ----------------------------------
+    // 오른쪽 화살표 키를 눌렀을 때 오른쪽으로 이동
+    // 오르쪽 화살표 키를 눌렀을 때
+    if(key == ConsoleKey.RightArrow)
+    {
+        // 오른쪽으로 이동, 콘솔 범위를 벗어나면 예외 발생
+        //playerX += 1;
+
+        //if(playerX == boxX - 1 && playerY == boxY)
+        //{
+        //    boxX = boxX < 15 ? boxX + 1 : boxX;
+        //}
+
+        // 오른쪽으로 이동
+        playerX = Math.Min(playerX + 1, 15); 
+    }
+
+    // 키 입력 구현할 때는 보통 if else 말고 if만 사용해서 각 키에 대한 동작을 구현한다.
+    if(key == ConsoleKey.UpArrow)
+    {
+        //playerY -= 1;
+
+        //if(playerY == boxY + 1 && playerX == boxX)
+        //{
+        //    boxY = boxY > 1 ? boxY - 1 : boxY;
+        //}
+
+        playerY = Math.Max(0, playerY - 1);
+    }
+
+    if(key == ConsoleKey.DownArrow)
+    {
+        //playerY += 1;
+
+        //if(playerX == boxX - 1 && playerY == boxY)
+        //{
+        //    boxY = boxY < 10 ? boxY + 1 : boxY;
+        //}
+
+        playerY = Math.Min(playerY + 1, 10);
+    }
+
+    if(key == ConsoleKey.LeftArrow)
+    {
+        //playerX -= 1;
+
+        //if(playerX == boxX + 1 && playerY == boxY)
+        //{
+        //    boxX = boxX > 0 ? boxX - 1 : boxX;
+        //}
+
+        playerX = Math.Max(0, playerX - 1);
+    }
+
+
+}
