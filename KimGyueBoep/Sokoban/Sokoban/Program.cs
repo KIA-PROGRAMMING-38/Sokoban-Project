210ae52b92015f68f200f1c90aeb638234f34b21
Qyebee99 <kim4188123@naver.com>
2022-12-27 19:20:01 +0900

    프로그램 초기 세팅

--- KimGyueBoep/Sokoban/Sokoban/Program.cs
+++ KimGyueBoep/Sokoban/Sokoban/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 초기 세팅
+using System.Runtime.CompilerServices;
+
+Console.ResetColor();                                 // 컬러를 초기화한다.
+Console.CursorVisible = false;                        // 커서를 숨긴다.
+Console.Title = "소코반 프로젝트";                     // 타이틀을 설정한다.
+Console.BackgroundColor = ConsoleColor.Green;         // 배경색을 설정한다.
+Console.ForegroundColor = ConsoleColor.Red;           // 글꼴색을 설정한다.
+Console.Clear();                                     // 출력된 모든 내용을 지운다.
