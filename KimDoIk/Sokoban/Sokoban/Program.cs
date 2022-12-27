63d7eba43bf834cbc65b0f77474afb42c1b24e70
kimdoik <anemone955@naver.com>
2022-12-27 19:21:57 +0900

    초기세팅

--- KimDoIk/Sokoban/Sokoban/Program.cs
+++ KimDoIk/Sokoban/Sokoban/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 초기 세팅
+Console.ResetColor();
+Console.CursorVisible = false; // 커서를 숨긴다. 블리언 타입입니다.
+Console.Title = "김도익 훈남"; // 타이틀을 설정한다.
+Console.BackgroundColor = ConsoleColor.DarkBlue; // 배경색을 설정한다.
+Console.ForegroundColor = ConsoleColor.White; // 글꼴색을 설정한다.
+Console.Clear(); // 출력된 모든 내용을 지운다.
+
