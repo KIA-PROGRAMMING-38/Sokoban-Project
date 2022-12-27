112351ef91431c7eed8a1474882b70f076aab5ad
stalepatt <cheeeedy@gmail.com>
2022-12-27 19:19:55 +0900

    프로그램 초기 세팅

--- youtaewoo/Sokoban/Sokoban/Program.cs
+++ youtaewoo/Sokoban/Sokoban/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//초기 세팅
+Console.ResetColor(); // 컬러를 초기화한다.
+Console.CursorVisible = false; // 커서 숨기기
+Console.Title = "미래의babaisyou"; // 타이틀을 설정한다.
+Console.BackgroundColor = ConsoleColor.DarkGreen; // 배경색을 설정한다.
+Console.ForegroundColor = ConsoleColor.White; //글꼴색을 설정한다.
+Console.Clear(); // 출력된 모든 내용을 지운다.
+
+//int x = 12;
+//int y = 6;
+//Console.SetCursorPosition(x, y);
+//Console.Write("B"); //여기서 개행하면 안된다.
+
+ConsoleKeyInfo KeyInfo = Console.ReadKey();
+Console.WriteLine(KeyInfo.Key);
