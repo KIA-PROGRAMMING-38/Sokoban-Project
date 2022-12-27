fc20fb145a05b796820fcb7700f9de673d5166aa
password-kim <galaxykim0611@gmail.com>
2022-12-27 19:21:26 +0900

    프로그램 초기 세팅

--- YUNHA/Sokoban/Sokoban/Program.cs
+++ YUNHA/Sokoban/Sokoban/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 초기 세팅
+Console.ResetColor();                                   // 컬러를 초기화한다.
+Console.CursorVisible = false;                          // 커서를 숨긴다.
+Console.Title = "Sokoban";                              // 타이틀을 설정한다.
+Console.BackgroundColor = ConsoleColor.DarkBlue;        // 배경색을 설정한다.
+Console.ForegroundColor = ConsoleColor.Yellow;          // 글꼴색을 설정한다.
+Console.Clear();                                        // 출력된 모든 내용을 지운다.
+
+
