25d8f80b037561fda28c4216b72a6705a74c1a94
Nameless1004 <jjho1029@gmail.com>
2022-12-27 19:20:00 +0900

    프로그램 초기 세팅

--- JungJaeho/Sokoban/Sokoban/Program.cs
+++ JungJaeho/Sokoban/Sokoban/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 초기세팅
+Console.ResetColor();                               // 컬러 초기화
+Console.CursorVisible = false;                      // 커서 비활성화
+Console.Title = "던전앤파이터";                     // 타이틀 설정
+Console.BackgroundColor = ConsoleColor.Gray;        // 배경색 설정
+Console.ForegroundColor = ConsoleColor.DarkCyan;    // 전경색 설정
+Console.Clear();                                    // 콘솔창 클리어
+/*-------------------------------------------------*/
