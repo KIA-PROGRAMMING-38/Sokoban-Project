7e45323ec0e28b31f532895881bdee7e9d12cd8b
stalepatt <cheeeedy@gmail.com>
2022-12-27 20:37:39 +0900

    플레이어 이동 구현

--- youtaewoo/Sokoban/Sokoban/Program.cs
+++ youtaewoo/Sokoban/Sokoban/Program.cs
@@ -6,10 +6,78 @@ Console.BackgroundColor = ConsoleColor.DarkGreen; // 배경색을 설정한다.
 Console.ForegroundColor = ConsoleColor.White; //글꼴색을 설정한다.
 Console.Clear(); // 출력된 모든 내용을 지운다.
 
-//int x = 12;
-//int y = 6;
-//Console.SetCursorPosition(x, y);
-//Console.Write("B"); //여기서 개행하면 안된다.
 
-ConsoleKeyInfo KeyInfo = Console.ReadKey();
-Console.WriteLine(KeyInfo.Key);
+int PlayerX = 0;
+int PlayerY = 0; // 얘는 바깥에다 해줘야한다. while문 안에 있으면 이동이 안된다.
+int BoxX = 5;
+int BoxY = 10;
+// 게임 루프 == 프레임(frame)
+while (true)
+{
+    // 이전 프레임을 지운다.
+    Console.Clear();
+    //---------------------------------Render--------------------------------
+    // 플레이어 출력하기
+    
+    Console.SetCursorPosition(PlayerX, PlayerY);
+    Console.Write("B");
+
+    // 박스 출력하기
+    Console.SetCursorPosition(BoxX, BoxY);
+    Console.Write("O");
+
+    // 벽 만들기
+    string[,] wall = new string[30, 15];
+    int wallindexX = 0;
+    int wallindexY = 0;
+    if (wallindexX == 30 || wallindexY == 15)
+    {
+        wall[wallindexX, wallindexY] = "W";
+        Console.WriteLine(wall);
+    }
+    else
+        wall[wallindexX, wallindexY] = "";
+        
+
+
+    //------------------------------ProcessInput-----------------------------
+    ConsoleKey key = Console.ReadKey().Key;
+
+    //---------------------------------Update--------------------------------
+    //오른쪽 화살표 키를 눌렀을 때를 먼저 한정해줄 것이다.
+    if (key == ConsoleKey.RightArrow && PlayerX < 30)
+    {
+        //오른쪽으로 이동
+        PlayerX += 1;
+        if (PlayerX == BoxX && PlayerY == BoxY)
+        {
+            BoxX = PlayerX + 1;
+        }
+    }
+    if (key == ConsoleKey.LeftArrow && PlayerX > 0) 
+    {
+        PlayerX -= 1;
+        if (PlayerX == BoxX && PlayerY == BoxY)
+        {
+            BoxX = PlayerX - 1;
+        }
+    }
+    if (key == ConsoleKey.DownArrow && PlayerY < 15)
+    {
+        PlayerY += 1;
+        if (PlayerX == BoxX && PlayerY == BoxY)
+        {
+            BoxY = PlayerY + 1;
+        }
+    }
+    if (key == ConsoleKey.UpArrow && PlayerY > 0) 
+    {
+        PlayerY -= 1;
+        if (PlayerX == BoxX && PlayerY == BoxY)
+        {
+            BoxY = PlayerY - 1;
+        }
+    }
+    
+
+}
