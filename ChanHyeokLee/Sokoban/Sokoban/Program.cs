55d677a4569c38674ac368ec40e4a9d377f149f7
dlcksgur4955@gmail.com <lees5402@naver.com>
2022-12-28 02:29:44 +0900

    ADD player, box implementation

--- ChanHyeokLee/Sokoban/Sokoban/Program.cs
+++ ChanHyeokLee/Sokoban/Sokoban/Program.cs
@@ -6,17 +6,92 @@ Console.BackgroundColor = ConsoleColor.Red; // 배경색을 설정한다.
 Console.ForegroundColor = ConsoleColor.White; // 글꼴 색을 설정한다.
 Console.Clear(); // 출력된 모든 내용을 지운다.
 
-//int x = 10;
-//int y = 5;
-//Console.SetCursorPosition(x, y);
-//Console.Write("L");
-ConsoleKeyInfo keyInfo = Console.ReadKey();
-Console.WriteLine(keyInfo.Key);
-/*while(true)
+int playerX = 0;
+int playerY = 0;
+int objectX = 5;
+int objectY = 5;
+int moveX = 0;
+int moveY = 0;
+
+
+
+
+//게임 루프 == 프레임(Frame)
+while (true)
 {
-    Processinput0; // 입력을 처리한다.
-    Update(); // 게임 데이터를 업데이트한다.
-    Render(); // 업데이트 된 데이터를 바탕으로 화면에 출력한다.
-}*/
+    Console.Clear();
+    //맵 가로 15 세로 10
+    // ------------------------------ Render ------------------------------
+    // 플레이어 출력하기
+    Console.SetCursorPosition(playerX, playerY);
+    Console.Write("L");
+    // 박스 출력하기
+    Console.SetCursorPosition(objectX, objectY);
+    Console.Write("O");
+    
+    
+    
+
+    // ------------------------------ ProcessInput ------------------------------
+    ConsoleKey key = Console.ReadKey().Key;
+
+    // ------------------------------ Update ------------------------------
+    
+    // 오른쪽 화살표 키를 눌렀을 때
+    if (key == ConsoleKey.RightArrow)
+    {
+        
+        playerX = Math.Min(playerX + 1, 15);
+        if(objectX == playerX && objectY == playerY)
+        {
+            if (objectX < 15)
+                objectX += 1;
+            else
+                playerX -= 1;
+        }
+    }
+    // 왼쪽 화살표 키를 눌렀을 때
+    if (key == ConsoleKey.LeftArrow)
+    {
+        playerX = Math.Max(0, playerX - 1);
+        if(objectX == playerX && objectY == playerY)
+        {
+            if (0 < objectX) //.......
+                objectX -= 1;
+            else
+                playerX += 1;
+        }
+    }
+    // 위쪽 화살표 키를 눌렀을 때
+    if (key == ConsoleKey.UpArrow)
+    {
+        playerY = Math.Max(0, playerY - 1);
+        if(objectY == playerY && objectX == playerX)
+        {
+            if (0 < objectY)
+                objectY -= 1;
+            else
+                playerY += 1;
+        }
+    }
+    // 아래로 움질일 때
+    if(key == ConsoleKey.DownArrow)
+    {
+        playerY = Math.Min(playerY + 1, 10);
+        if (objectY == playerY && objectX == playerX)
+        {
+            if (objectY < 10)
+                objectY += 1;
+            else
+                playerY -= 1;
+        }
+    }
+   
+        
+    
+    
+    
+}
+
 
 
