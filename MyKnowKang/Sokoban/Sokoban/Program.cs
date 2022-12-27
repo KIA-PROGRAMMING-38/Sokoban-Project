348d2edff691c52a1b6c49bbe9a327cd793473db
kangminhou <kangminhou@naver.com>
2022-12-27 19:22:17 +0900

    KMH Project DefaultSetting

--- MyKnowKang/Sokoban/Sokoban/Program.cs
+++ MyKnowKang/Sokoban/Sokoban/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 
-namespace Sokoban
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        // 초기 세팅..
+        Console.ResetColor();                               // 컬러를 초기화한다..
+        Console.CursorVisible = false;                      // 커서를 숨긴다..
+        Console.Title = "Welcome To Liverpool";             // 타이틀을 설정한다..
+        Console.BackgroundColor = ConsoleColor.DarkRed;     // Background 색을 설정한다..
+        Console.ForegroundColor = ConsoleColor.White;       // 글꼴색을 설정한다..
+        Console.Clear();                                    // 출력된 모든 내용을 지운다..
+
+        // 콘솔 최대 사이즈 119, 29..
     }
 }
