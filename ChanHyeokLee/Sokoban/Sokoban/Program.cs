19cdedc8a131dcf39d4937bf2f6cde8e58c8f978
dlcksgur4955@gmail.com <lees5402@naver.com>
2022-12-27 19:20:44 +0900

    프로그램 초기 세팅

--- ChanHyeokLee/Sokoban/Sokoban/Program.cs
+++ ChanHyeokLee/Sokoban/Sokoban/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//초기 세팅
+Console.ResetColor(); //컬러를 초기화한다.
+Console.CursorVisible = false; // 커서를 없애준다.
+Console.Title = "소코반"; //타이틀 이름
+Console.BackgroundColor = ConsoleColor.Red; // 배경색을 설정한다.
+Console.ForegroundColor = ConsoleColor.White; // 글꼴 색을 설정한다.
+Console.Clear(); // 출력된 모든 내용을 지운다.
+
+//int x = 10;
+//int y = 5;
+//Console.SetCursorPosition(x, y);
+//Console.Write("L");
+ConsoleKeyInfo keyInfo = Console.ReadKey();
+Console.WriteLine(keyInfo.Key);
+/*while(true)
+{
+    Processinput0; // 입력을 처리한다.
+    Update(); // 게임 데이터를 업데이트한다.
+    Render(); // 업데이트 된 데이터를 바탕으로 화면에 출력한다.
+}*/
+
+
