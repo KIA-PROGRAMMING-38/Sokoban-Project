e25e3d6ae79e4a8f02479395edd66a8832319a40
JS <argentdarae@gmail.com>
2022-12-27 19:22:30 +0900

    Program Initial Setting

--- JOO JIN SOO/Sokoban/Sokoban/Program.cs
+++ JOO JIN SOO/Sokoban/Sokoban/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 초기 세팅
+Console.ResetColor();                              // 컬러를 초기화한다
+Console.CursorVisible = false;                     // 커서를 켜고 끄는 것, 불리언 타입이다
+Console.Title = "홍성재의 파이어펀치";               // 타이틀 명을 바꾸어주는 이름
+Console.BackgroundColor = ConsoleColor.DarkGreen;  // 배경색을 설정한다
+Console.ForegroundColor = ConsoleColor.Yellow;     // 글꼴색을 설정한다
+Console.Clear();                                   // 출력된 모든 내용을 지운다
