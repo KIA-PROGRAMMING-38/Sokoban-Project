774c7e7a3b852e1562c9f23db3a280ef1b7693c8
Mati_as <gongminseok505@gmail.com>
2022-12-27 19:21:58 +0900

    프로그램 초기 셋팅

--- Mati_as/Sokoban/Program.cs
+++ Mati_as/Sokoban/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// initia Setting
+Console.ResetColor(); //default color
+Console.CursorVisible = false; //(make Cursor invisible)
+Console.Title = "뀨뀨 는 구구구구";// set title
+Console.BackgroundColor = ConsoleColor.Cyan; //set background
+Console.ForegroundColor = ConsoleColor.Yellow; // set the color of  letters
+Console.Clear();   //remove all the content.
+
