f3023400901ee285d474d299abc1963be7d613d1
Yoo-JaeKwang <jaeu4100@gmail.com>
2022-12-27 19:22:06 +0900

    프로그램 초기 세팅

--- Yoo-JaeKwang/Sokoban/Sokoban/Program.cs
+++ Yoo-JaeKwang/Sokoban/Sokoban/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 초기 세팅
+Console.ResetColor(); // 컬러를 초기화한다.
+Console.CursorVisible = false;
+Console.Title = "경이루 아카데미";
+Console.BackgroundColor = ConsoleColor.Magenta;
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Clear();
+
+//int x = 10;
+//int y = 5;
+//Console.SetCursorPosition(x, y);
+//Console.Write("P");
+
+ConsoleKeyInfo keyInfo = Console.ReadKey();
+Console.WriteLine(keyInfo.Key);
