11638280fba472268a29e4745b19a088f8e7839b
0105KCY <ggbbsing@gmail.com>
2022-12-28 21:47:33 +0900

    주석 정리

--- KimChangYeon/Sokoban/Sokoban/Program.cs
+++ KimChangYeon/Sokoban/Sokoban/Program.cs
@@ -9,11 +9,8 @@
     }
     class Program
     {
-
         static void Main()
         {
-
-
             //초기 세팅
             Console.ResetColor();                            // 컬러를 초기화 한다
             Console.CursorVisible = false;                   // 커서를 숨긴다
@@ -23,17 +20,10 @@
             Console.Clear();                                 //출력된 모든 내용을 지운다.
 
             // 기호 상수
-            //맵의 가로 범위, 세로 범위
             const int MAP_MIN_X = 0;
             const int MAP_MIN_Y = 0;
             const int MAP_MAX_X = 15;
             const int MAP_MAX_Y = 10;
-            //플레이어의 기호(string literal)
-            //박스의 기호(string literal)
-            //플레이어의 이동 방향
-            //플레이어의 초기 좌표
-            // 박스의 초기 좌표
-
             const int INITIAL_PLAYER_X = 0;
             const int INITIAL_PLAYER_Y = 0;
             const int INITIAL_BOX_X = 3;
@@ -44,12 +34,10 @@
             int Box1X = INITIAL_BOX_X;
             int Box1Y = INITIAL_BOX_Y;
 
-
             PLAYER_DIRECTION playerDIR = new PLAYER_DIRECTION();
 
             //15 X 10
             // 게임 루프 == 프레임(Frame)
-
             while (true)
             {
                 Console.Clear();
@@ -86,26 +74,7 @@
                     playerDIR = PLAYER_DIRECTION.UP;
                     
                 }
-                //if (PlayerX == Box1X && PlayerY == Box1Y && PlayerDir == 1)
-                //{
-                //    PlayerX = Math.Min(PlayerX, HorizonMax - 1);
-                //    Box1X = Math.Min(Box1X + 1, HorizonMax);
-                //}
-                //if (PlayerX == Box1X && PlayerY == Box1Y && PlayerDir == 2)
-                //{
-                //    PlayerX = Math.Max(PlayerX, 1);
-                //    Box1X = Math.Max(Box1X - 1, 0);
-                //}
-                //if (PlayerX == Box1X && PlayerY == Box1Y && PlayerDir == 4)
-                //{
-                //    PlayerY = Math.Max(PlayerY, 1);
-                //    Box1Y = Math.Max(Box1Y - 1, 0);
-                //}
-                //if (PlayerX == Box1X && PlayerY == Box1Y && PlayerDir == 3)
-                //{
-                //    PlayerY = Math.Min(PlayerY, VerticalMax - 1);
-                //    Box1Y = Math.Min(Box1Y + 1, VerticalMax);
-                //}
+  
                 if (PlayerX == Box1X && PlayerY == Box1Y)
                 {
                     switch (playerDIR)
@@ -134,8 +103,6 @@
 
                     }
                 }
-
-
             }
         }
     }
