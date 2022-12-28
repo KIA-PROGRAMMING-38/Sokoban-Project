6c6bb287fa6d8cba902e9c5ad359b7a95018efe4
stalepatt <cheeeedy@gmail.com>
2022-12-28 16:51:13 +0900

    코드 리팩토링 - SWITCH 문

--- youtaewoo/Sokoban/Sokoban/Program.cs
+++ youtaewoo/Sokoban/Sokoban/Program.cs
@@ -7,12 +7,12 @@ Console.ForegroundColor = ConsoleColor.White; //글꼴색을 설정한다.
 Console.Clear(); // 출력된 모든 내용을 지운다.
 
 // 플레이어 좌표 설정
-int PlayerX = 0;
-int PlayerY = 0; // 얘는 바깥에다 해줘야한다. while문 안에 있으면 이동이 안된다.
-
+int playerX = 0;
+int playerY = 0; // 얘는 바깥에다 해줘야한다. while문 안에 있으면 이동이 안된다.
+int playerDirection = 0;
 // 박스 좌표 설정
-int BoxX = 5;
-int BoxY = 10;
+int boxX = 5;
+int boxY = 10;
 
 // 게임 루프 == 프레임(frame)
 while (true)
@@ -22,11 +22,11 @@ while (true)
     //---------------------------------Render--------------------------------
     // 플레이어 출력하기
     
-    Console.SetCursorPosition(PlayerX, PlayerY);
+    Console.SetCursorPosition(playerX, playerY);
     Console.Write("B");
 
     // 박스 출력하기
-    Console.SetCursorPosition(BoxX, BoxY);
+    Console.SetCursorPosition(boxX, boxY);
     Console.Write("O");
 
     //------------------------------ProcessInput-----------------------------
@@ -34,50 +34,77 @@ while (true)
 
     //---------------------------------Update--------------------------------
     //오른쪽 화살표 키를 눌렀을 때를 먼저 한정해줄 것이다.
-    if (key == ConsoleKey.RightArrow && PlayerX < 30)
-    {
-        //오른쪽으로 이동
-        
-        if (PlayerX == BoxX - 1 && PlayerY == BoxY)
-        {
-            PlayerX = Math.Min(PlayerX + 1, 29);
-            BoxX = Math.Min(PlayerX + 1, 30);
-        }
-        else
-            PlayerX = Math.Min(PlayerX + 1, 30);
+    if (key == ConsoleKey.RightArrow && playerX < 15) //오른쪽 이동  
+    {                  
+            playerX = Math.Min(playerX + 1, 15);
+            playerDirection = 2;
     }
-    if (key == ConsoleKey.LeftArrow && PlayerX > 0) 
-    {
-        
-        if (PlayerX == BoxX + 1 && PlayerY == BoxY)
-        {
-            PlayerX = Math.Max(1, PlayerX - 1);
-            BoxX = Math.Max(0, PlayerX - 1);
-        }
-        else
-            PlayerX = Math.Max(0, PlayerX - 1);
-    }
-    if (key == ConsoleKey.DownArrow && PlayerY < 15)
-    {
-        
-        if (PlayerX == BoxX && PlayerY == BoxY - 1)
-        {
-            PlayerY = Math.Min(PlayerY + 1, 14);
-            BoxY = Math.Min(PlayerY + 1, 15);
-        }
-        else
-            PlayerY = Math.Min(PlayerY + 1, 15);
-    }
-    if (key == ConsoleKey.UpArrow && PlayerY > 0) 
-    {
-        
-        if (PlayerX == BoxX && PlayerY == BoxY + 1 )
-        {
-            PlayerY = Math.Max(1, PlayerY - 1);
-            BoxY = Math.Max(0, PlayerY - 1);
-        }
-        else
-            PlayerY = Math.Max(0, PlayerY - 1);
-    }   
 
+    if (key == ConsoleKey.LeftArrow && playerX > 0) //왼쪽 이동
+    {        
+            playerX = Math.Max(0, playerX - 1);
+            playerDirection = 1;
+    }
+    if (key == ConsoleKey.DownArrow && playerY < 15) //아래 이동
+    {        
+            playerY = Math.Min(playerY + 1, 15);
+            playerDirection = 4;
+    }
+    if (key == ConsoleKey.UpArrow && playerY > 0) //위 이동
+    {        
+            playerY = Math.Max(0, playerY - 1);
+            playerDirection = 3;
+    }   
+    // 박스 업데이트
+    if (playerX == boxX && playerY == boxY) // 플레이어가 이동하고 나니 박스가 있는 것
+    {
+        //박스를 움직여주면 된다.
+        switch(playerDirection)
+        {
+            case 1: // 박스 왼쪽
+                if (boxX == 0)
+                {
+                    playerX = 1;
+                }
+                else
+                {
+                    boxX = boxX - 1;
+                }
+                break;
+            case 2: // 박스 오른쪽
+                if (boxX == 15)
+                {
+                    playerX = 14;
+                }
+                else
+                {
+                    boxX = boxX + 1;
+                }
+                break;
+            case 3: // 박스 위쪽
+                if (boxY == 0)
+                {
+                    playerY = 1;
+                }
+                else
+                {
+                    boxY = boxY - 1;
+                }
+                break;
+            case 4: // 박스 아래쪽
+                if (boxY == 15)
+                {
+                    playerY = 14;
+                }
+                else
+                {
+                    boxY = boxY + 1;
+                }
+                break;
+            default:
+                Console.Clear();
+                Console.WriteLine($"[Error] 플레이어의 이동 방향이 잘못되었습니다.");
+                return;
+        }
+    }
 }
