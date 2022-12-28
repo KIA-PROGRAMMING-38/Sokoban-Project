6f1b015c1057d84c27281d4bce96ee85309a60d3
sjhong <lunar105630@gmail.com>
2022-12-28 18:14:01 +0900

    소코반 크기 및 방향 상수화

--- SJHong/Sokoban/Sokoban/Program.cs
+++ SJHong/Sokoban/Sokoban/Program.cs
@@ -1,6 +1,23 @@
 ﻿using System;
 class Program
 {
+    enum Direction
+    {   
+        None,
+        Right,
+        Left,
+        Down,
+        Up
+    }
+
+    enum Size
+    {
+        None,
+        RightEnd = 15,
+        LeftEnd = 0,
+        DownEnd = 10,
+        UpEnd = 0
+    }
     static void Main()
     {
         Console.ResetColor();
@@ -12,7 +29,7 @@ class Program
 
         int playerX = 0;
         int playerY = 0;
-        int playerDirection = 0; 
+        Direction playerDirection = default;
 
         int boxX = 5;
         int boxY = 5;
@@ -40,69 +57,69 @@ class Program
 
             if (Key == ConsoleKey.RightArrow)
             {
-                playerX = Math.Min(playerX + 1, 15);
-                playerDirection = 1;
+                playerX = Math.Min(playerX + 1, (int)Size.RightEnd);
+                playerDirection = Direction.Right;
 
             }
 
             if (Key == ConsoleKey.LeftArrow)
             {
-                playerX = Math.Max(0, playerX - 1);
-                playerDirection = 2;
+                playerX = Math.Max((int)Size.LeftEnd, playerX - 1);
+                playerDirection = Direction.Left;
 
             }
 
             if (Key == ConsoleKey.DownArrow)
             {
-                playerY = Math.Min(playerY + 1, 15);
-                playerDirection = 3;
+                playerY = Math.Min(playerY + 1, (int)Size.DownEnd);
+                playerDirection = Direction.Down;
 
             }
 
             if (Key == ConsoleKey.UpArrow)
             {
-                playerY = Math.Max(0, playerY - 1);
-                playerDirection = 4;
+                playerY = Math.Max((int)Size.UpEnd, playerY - 1);
+                playerDirection = Direction.Up;
 
             }
             if (boxX == playerX && boxY == playerY)
             {
-                switch(playerDirection)
+                switch (playerDirection)
                 {
-                    case 1:
-                        if(boxX == 15)
+                    case Direction.Right:
+                        if (boxX == (int)Size.RightEnd)
                         {
-                            playerX = 14;
+                            playerX = (int)Size.RightEnd - 1;
                         }
                         else
                         {
                             ++boxX;
                         }
                         break;
-                    case 2:
-                        if(boxX == 0)
+                    case Direction.Left:
+                        if (boxX == (int)Size.LeftEnd)
                         {
-                            playerX = 1;
+                            playerX = (int)Size.LeftEnd + 1;
                         }
                         else
                         {
                             --boxX;
                         }
                         break;
-                    case 3:
-                        if(boxY == 15)
+                    case Direction.Down:
+                        if (boxY == (int)Size.DownEnd)
                         {
-                            playerY = 14;
+                            playerY = (int)Size.DownEnd - 1;
                         }
                         else
                         {
                             ++boxY;
                         }
                         break;
-                    case 4:
-                        if(boxY == 0)
+                    case Direction.Up:
+                        if (boxY == (int)Size.UpEnd)
                         {
-                            playerY = 1;
+                            playerY = (int)Size.UpEnd + 1;
                         }
                         else
                         {
@@ -112,7 +129,7 @@ class Program
                     default:
                         Console.Clear();
                         Console.WriteLine($"[Error] 플레이어의 이동방향이 잘못되었습니다. {playerDirection}");
-                        
+
                         return;
                 }
 
