10d08f31ca9747336ff146e631ad99e8fc236c39
leejw <krebye@naver.com>
2022-12-29 15:31:20 +0900

    박스 이동 구현 ,열거형 정리

--- leejinwoo/Sokoban/Sokoban/Program.cs
+++ leejinwoo/Sokoban/Sokoban/Program.cs
@@ -1,7 +1,15 @@
 ﻿using System;
 namespace Sokoban
 {
-    
+    enum Direction
+    {
+        None,
+        Left,
+        Right,
+        Up,
+        Down
+    }
+
 
     class Program
     {
@@ -19,38 +27,39 @@ namespace Sokoban
             Console.Clear();
 
             // 기호 상수
-            // 맵 가로범위, 세로범위
-            //const int MAP_MIN_X = 0;
-            //const int MAP_MIN_Y = 0;
-            //const int MAP_MAX_X = 15;
-            //const int MAP_MAX_Y = 10;
+            //맵 가로범위, 세로범위
+            const int MAP_MIN_X = 0;
+            const int MAP_MIN_Y = 0;
+            const int MAP_MAX_X = 15;
+            const int MAP_MAX_Y = 10;
 
-            //// 플레이어의 이동 방향
-            //const int DIRECTION_LEFT = 1;
-            //const int DIRECTION_RIGHT = 2;
-            //const int DIRECTION_UP = 3;
-            //const int DIRECTION_DOWN = 4;
+            // 플레이어의 이동 방향
+            const int DIRECTION_LEFT = 1;
+            const int DIRECTION_RIGHT = 2;
+            const int DIRECTION_UP = 3;
+            const int DIRECTION_DOWN = 4;
 
-            //// 플레이어의 초기 좌표
-            //const int INITIAL_PLAYER_X = 0;
-            //const int INITIAL_PLAYER_Y = 0;
-            //Direction playerDirection = Direction
-            //// 플레이어 기호 (string literal)
-            //const string PLAYER_STRING = "P";
-            //// 박스의 기호 (string literal)
-            //const string BOX_STRING = "B";
+            // 플레이어의 초기 좌표
+            const int INITIAL_PLAYER_X = 0;
+            const int INITIAL_PLAYER_Y = 0;
+            Direction playerDirection = 0;
+            // 플레이어 기호 (string literal)
+            const string PLAYER_STRING = "P";
+            // 박스의 기호 (string literal)
+            const string BOX_STRING = "B";
 
-            //// 박스의 초기 좌표
-            //const int INITIAL_BOX_X = 0;
-            //const int INITIAL_BOX_Y = 0;
+            // 박스의 초기 좌표
+            const int INITIAL_BOX_X = 5;
+            const int INITIAL_BOX_Y = 5;
 
 
             int playerX = 0;
             int playerY = 0;
+             // 0 : None, 1 : Left, 2 : Right, 3 : Up, 4 : Down [1, 4]
             //박스 좌표설정
             int boxX = 5;
             int boxY = 5;
-
+        
 
             // 가로 15 세로 10
             //게임 루프 == 프레임
@@ -70,32 +79,83 @@ namespace Sokoban
 
                 // ------------update--------------
                 //오른쪽 화살표키를 눌렀을때
-                if (key == ConsoleKey.RightArrow)
-                {
-                    playerX = Math.Min(15, playerX + 1);
-                }
-
                 if (key == ConsoleKey.LeftArrow)
                 {
-                    playerX = Math.Max(0, playerX - 1);
+                    playerX = Math.Max(MAP_MIN_X, playerX - 1);
+                    playerDirection = Direction.Left;
+                }
+
+                if (key == ConsoleKey.RightArrow)
+                {
+                    playerX = Math.Min(MAP_MAX_X, playerX + 1);
+                    playerDirection = Direction.Right;
                 }
                 if (key == ConsoleKey.UpArrow)
                 {
-                    playerY = Math.Max(0, playerY - 1);
+                    playerY = Math.Max(MAP_MIN_Y, playerY - 1);
+                    playerDirection = Direction.Up;
                 }
                 if (key == ConsoleKey.DownArrow)
                 {
-                    playerY = Math.Min(10, playerY + 1);
+                    playerY = Math.Min(MAP_MAX_Y, playerY + 1);
+                    playerDirection = Direction.Down;
                 }
 
-                
+                if (playerX == boxX && playerY == boxY) // 플레이어가 이동하고나니 박스가 있네?
+                {
+                    // 박스를 움직여주면 됨.
+                    switch (playerDirection)
+                    {
+                        case Direction.Left: // 플레이어가 왼쪽으로 이동 중
+                            if (boxX == MAP_MIN_X) // 박스가 왼쪽 끝에 있다면?
+                            {
+                                playerX = 1;
+                            }
+                            else
+                            {
+                                boxX = boxX - 1;
+                            }
+                            break;
+                        case Direction.Right: // 플레이어가 오른쪽으로 이동 중
+                            if (boxX == MAP_MAX_X) // 박스가 오른쪽 끝에 있다면?
+                            {
+                                playerX = 14;
+                            }
+                            else
+                            {
+                                boxX = boxX + 1;
+                            }
+                            break;
+                        case Direction.Up: // 플레이어가 위로 이동 중
+                            if (boxY == MAP_MIN_Y)
+                            {
+                                playerY = 1;
+                            }
+                            else
+                            {
+                                boxY = boxY - 1;
+                            }
+                            break;
+                        case Direction.Down: // 플레이어가 아래로 이동 중
+                            if (boxY == MAP_MAX_Y)
+                            {
+                                playerY = 9;
+                            }
+                            else
+                            {
+                                boxY = boxY + 1;
+                            }
+                            break;
+                        default: //
+                            Console.Clear();
+                            Console.WriteLine($"[Error] 플레이어의 이동 방향이 잘못되었습니다. {playerDirection}");
+
+                            return; // 프로그램 종료
+                    }
+
+                }
+
             }
-
-
-
-
-
-
         }
     }
 }
