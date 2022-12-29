dd287bfaf5e5f4e33573b1b3b5af3bb9818f042d
huui <gmlwl0408@gmail.com>
2022-12-29 15:51:29 +0900

    상수로 바꾸기 self

--- Sokoban/Program.cs
+++ Sokoban/Program.cs
@@ -1,125 +1,173 @@
-﻿// 초기 세팅
-Console.ResetColor();                       // 컬러를 초기화한다.
-Console.CursorVisible = false;              // 커서를 보이지 않게 해준다. 맥은 좀 보일수도..
-Console.Title = "Sokoban";                  // 제목을 정해준다.
-Console.BackgroundColor = ConsoleColor.Blue; // 백그라운드 색깔을 설정해준다.
-Console.ForegroundColor = ConsoleColor.DarkGray; // 글꼴 색깔 지정.
-Console.Clear(); // 출력된 모든 내용을 지운다.
-
-
-// 플레이어 좌표 설정
-int playerX = 0;
-int playerY = 0;
-
-//self
-int boxX = 5;
-int boxY = 5;
-
-int playerDirection = 0;
-
-
-// 게임 루프 == 프레임(Frame)
-while (true)
-
-{   // 이전 프레임을 지운다.
-    Console.Clear();
-    // ---------------------Render----------------
-
-    Console.SetCursorPosition(playerX, playerY);
-    Console.Write("H");
-
-    // self
-    Console.SetCursorPosition(boxX, boxY);
-    Console.Write("O");
-
-    // ---------------------ProcessInput-----------
-    ConsoleKey key = Console.ReadKey().Key;
-    // ---------------------Update-----------------
-
-    if (key == ConsoleKey.LeftArrow)
+﻿namespace Sokoban
+{
+    // 열거형
+    enum Direction
     {
-        playerX = Math.Max(0, playerX - 1);
-        playerDirection = 1;
-       
+        Left = 1,
+        Right = 2,
+        Up = 3,
+        Down = 4
     }
-
-    // 오른쪽 화살표키를 눌렀을 때
-    if (key == ConsoleKey.RightArrow)
+    
+    class Program
     {
-        // 오른쪽으로 이동 => 누를때마다 1씩 이동한다.
-        playerX = Math.Min(playerX + 1, 15);
-        playerDirection = 2;
-   
-    }
-
-    if (key == ConsoleKey.UpArrow)
-    {
-        playerY = Math.Max(0, playerY - 1);
-        playerDirection = 3;
-     
-    }
-
-    if (key == ConsoleKey.DownArrow)
-    {
-        playerY = Math.Min(playerY + 1, 10);
-        playerDirection = 4;
-     
-    }
-
-    //박스 update
-    if (playerX == boxX && playerY == boxY)
-    {
-        switch (playerDirection)
+        
+        static void Main()
         {
-            case 1: //플레이어가 왼쪽으로 이동
-               if (boxX == 0)
-                {
-                    playerX = 1;
-                }
-                else
-                {
-                    boxX -= 1;
-                }
-                break;
-            case 2: //플레이어가 오른쪽으로 이동
-                if (boxX == 15)
-                {
-                    playerX = 14;
-                }
-                else
-                {
-                    boxX += 1;
-                }
-                break;
-            case 3: //플레이어가 위로 이동
-                if (boxY == 0)
-                {
-                    playerY = 1;
-                }
-                else
-                {
-                    boxY -= 1;
-                }
-                break;
-            case 4:
-                if (boxY == 10)
-                {
-                    playerY = 9;
-                }
-                else
-                {
-                    boxY += 1;
-                }
-                break;
-            default:
+
+            // 초기 세팅
+            Console.ResetColor();                       // 컬러를 초기화한다.
+            Console.CursorVisible = false;              // 커서를 보이지 않게 해준다. 맥은 좀 보일수도..
+            Console.Title = "Sokoban";                  // 제목을 정해준다.
+            Console.BackgroundColor = ConsoleColor.Blue; // 백그라운드 색깔을 설정해준다.
+            Console.ForegroundColor = ConsoleColor.DarkGray; // 글꼴 색깔 지정.
+            Console.Clear(); // 출력된 모든 내용을 지운다.
+
+            const int INITIAL_PLAYER_X = 0;
+            const int INITIAL_PLAYER_Y = 0;
+
+            // 플레이어 좌표 설정
+            int playerX = INITIAL_PLAYER_X;
+            int playerY = INITIAL_PLAYER_Y;
+
+            
+            // box 좌표 설정
+            int boxX = 5;
+            int boxY = 5;
+
+            
+
+            Direction playerDirection = Direction.Down;
+
+            //map
+            const int MAP_MIN_X = 0;
+            const int MAP_MIN_Y = 0;
+            const int MAP_MAX_X = 15;
+            const int MAP_MAX_Y = 10;
+
+            // 플레이어의 기호
+            const string PLAYER_STRING = "H";
+
+            // 박스의 기호
+            const string BOX_STRING = "O";
+            //플레이어의 초기 좌포
+            
+
+
+
+
+            // 게임 루프 == 프레임(Frame)
+            while (true)
+
+            {   // 이전 프레임을 지운다.
                 Console.Clear();
-                Console.WriteLine($"[Error]{playerDirection}");
-                break;
-        }
+                // ---------------------Render----------------
+
+                Console.SetCursorPosition(playerX, playerY);
+                Console.Write(PLAYER_STRING);
+
+                // self
+                Console.SetCursorPosition(boxX, boxY);
+                Console.Write(BOX_STRING);
+
+                // ---------------------ProcessInput-----------
+                ConsoleKey key = Console.ReadKey().Key;
+                // ---------------------Update-----------------
+
+                if (key == ConsoleKey.LeftArrow)
+                {
+                    playerX = Math.Max(MAP_MIN_X, playerX - 1);
+                    playerDirection = Direction.Left;
+
+                }
+
+                // 오른쪽 화살표키를 눌렀을 때
+                if (key == ConsoleKey.RightArrow)
+                {
+                    // 오른쪽으로 이동 => 누를때마다 1씩 이동한다.
+                    playerX = Math.Min(playerX + 1, MAP_MAX_X);
+                    playerDirection = Direction.Right;
+
+                }
+
+                if (key == ConsoleKey.UpArrow)
+                {
+                    playerY = Math.Max(MAP_MIN_Y, playerY - 1);
+                    playerDirection = Direction.Up;
+
+                }
+
+                if (key == ConsoleKey.DownArrow)
+                {
+                    playerY = Math.Min(playerY + 1, MAP_MAX_Y);
+                    playerDirection = Direction.Up;
+
+                }
+
+                //박스 update
+                if (playerX == boxX && playerY == boxY)
+                {
+                    switch (playerDirection)
+                    {
+                        case Direction.Left: //플레이어가 왼쪽으로 이동
+                            if (boxX == MAP_MIN_X)
+                            {
+                                playerX = MAP_MIN_X + 1;
+                            }
+                            else
+                            {
+                                boxX -= 1;
+                            }
+                            break;
+                        case Direction.Right: //플레이어가 오른쪽으로 이동
+                            if (boxX == MAP_MAX_X)
+                            {
+                                playerX = MAP_MAX_X - 1;
+                            }
+                            else
+                            {
+                                boxX += 1;
+                            }
+                            break;
+                        case Direction.Up: //플레이어가 위로 이동
+                            if (boxY == MAP_MIN_Y)
+                            {
+                                playerY = MAP_MIN_Y + 1;
+                            }
+                            else
+                            {
+                                boxY -= 1;
+                            }
+                            break;
+                        case Direction.Down:
+                            if (boxY == MAP_MAX_Y)
+                            {
+                                playerY = MAP_MAX_Y - 1;
+                            }
+                            else
+                            {
+                                boxY += 1;
+                            }
+                            break;
+                        default:
+                            Console.Clear();
+                            Console.WriteLine($"[Error]{playerDirection}");
+                            break;
+                    }
+                }
+
+
+            }
+            
+
     }
-
-
 }
+}
+
+
+
+
+
 
 //int x = 20;
 //int y = 3;
