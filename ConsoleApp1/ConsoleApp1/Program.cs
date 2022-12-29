47b1977e8d392012a2c02497d43a03d3d4ee0401
Ricky <wooseok1995@gmail.com>
2022-12-29 20:31:44 +0900

    벽만들기 + 벽에 막히는거 구현

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,21 +1,51 @@
 ﻿
-/*
-string[] input = Console.ReadLine().Split(" "); // 10 5 입력 // input[0] = 10, input[1] = 5
 
-string[] input2 = Console.ReadLine().Split(" "); // 숫자열
+int save = 0;
+int[] saveset = new int[10]; // 나머지값을 저장하기 위한 배열을 만듦
+string test = "";
+int[] testset = new int[10]; // 나머지값을 비교하기 위해 배열을 만듦
+int count = 0;
 
-string answer = "";
-int length = input2.Length;
-
-Array.Sort(input2);
-
-Console.WriteLine(input2[0]);
-*/
-
-const int A = 0;
-
-if (10 > A)
+for (int i = 0; i < 10; ++i)
 {
-    Console.WriteLine("True");
-}
+    string input = Console.ReadLine();
+
+    int value = int.Parse(input);
+
+    // 나머지값 구하기
+
+    save = value % 42; // 1 2 3 4 5~~10을 42로 나머지를 구했다
+    saveset[i] = save; // 나머지를 배열로 만들었다. saveset[0] = 1, saveset[1] = 2, saveset[2] = 3 , saveset[3] = 4 ~~
+    test += save + " "; // test = "1 2 3 4 5 6 7 8 9 10"
+    testset[i] = save; // testset[0] = 1, testset[1] = 2, testset[2] = 3, testset[3] = 4 ~~~
+} 
+
+    for(int k = 0; k < 10 ;++k)
+    {
+        for (int m = 0; m < 9 ;++m)
+        {
+            if (saveset[k] != testset[m])
+            {
+            count++;
+            }
+        }
+        
+
+    }
+
+    Console.WriteLine(count);
+   
+
+
+
+
+
+
+
+
+    
+
+
+
+
 
--- Sokoban-Project/Sokoban-Project/Program.cs
+++ Sokoban-Project/Sokoban-Project/Program.cs
@@ -15,12 +15,6 @@
     }
 
 
-
-
-
-
-
-
     class program
     {
         static void Main()
@@ -33,36 +27,32 @@
             Console.ForegroundColor = ConsoleColor.Red; // 글씨의 색을 바꿔줌
             Console.Clear(); // 출력된 모든 내용을 지운다
 
+            
 
-
-
-            // 기호상수
-            // 맵의 가로범위, 세로범위 - O
-            // 플레이어 기호(string literal) - O
-            // 박스의 기호(string literal) - O
-            // 플레이어 이동방향 - O
-            // 플레이어의 초기좌표 - O
-            // 박스의 초기좌표 - O
-
-
-            const int MAP_MIN_X = 0;
-            const int MAP_MIN_Y = 0;
-            const int MAP_MAX_X = 15; // 맵의 가로범위
-            const int MAP_MAX_Y = 10; // 맵의 세로범위
+            const int MAP_MIN_X = 0; // 맵의 가로 최소
+            const int MAP_MIN_Y = 0; // 맵의 세로 최소
+            const int MAP_MAX_X = 15; // 맵의 가로 최대
+            const int MAP_MAX_Y = 10; // 맵의 세로 최대
 
             const string Player = "R"; // 플레이어 기호
             const string Box = "B"; // 박스 기호
+            
+            
+            const int INITIAL_WALL_X= 7; // 벽의 좌표
+            const int INITIAL_WALL_Y = 8;
+            const string WALL_STRING = "W";
+
+            // 1. 플레이어가 벽에 부딪혀야함
+            
+
+
+
+            // 2. 박스도 벽에 부딪혀야 함
 
             //  const int player_Direction = 0;
             // int playerDirection = player_Direction; // 0: None, 1: 왼쪽으로 이동중이였다(Left), 2: Right, 3: Up, 4: Down
 
             DIRECTION playerDirection = DIRECTION.NONE;
-
-            const int DirectionLeft = 1; // 플레이어 왼쪽 이동방향
-            const int DirectionRight = 2; // 플레이어 오른쪽 이동방향
-            const int DirectionUp = 3; // 플레이어 위쪽 이동방향
-            const int DirectionDown = 4; // 플레이어 아래쪽 이동방향
-
 
 
             const int player_X = 0; // Player_X를 굽이 playerx 로 바꾼 이유는 아래를 다 바꾸기 귀찮아서임...
@@ -79,8 +69,8 @@
             int boxY = Box_Y;
 
             // 가로 15, 세로 15
-
-
+           
+            
             
           
 
@@ -100,6 +90,9 @@
                 Console.SetCursorPosition(boxX, boxY);
                 Console.Write($"{Box}");
 
+                Console.SetCursorPosition(INITIAL_WALL_X, INITIAL_WALL_Y);
+                Console.Write(WALL_STRING);
+               
 
                 // ------------------------------------------ ProcessInput -----------------------------------------
 
@@ -109,38 +102,54 @@
 
                 // ------------------------------------------ Update -----------------------------------------------
                 // 플레이어 좌표 구하기
-                if (key == ConsoleKey.RightArrow)
+                if (key == ConsoleKey.RightArrow) // 오른쪽으로 이동할 때
                 {
-                    // 오른쪽으로 이동
+                    
                     playerX = Math.Min(playerX + 1, MAP_MAX_X);
                     playerDirection = DIRECTION.RIGHT;
+                    if(playerX==INITIAL_WALL_X && playerY == INITIAL_WALL_Y)
+                    {
+                        playerX = INITIAL_WALL_X - 1;
+                    }
                 }
 
-                if (key == ConsoleKey.LeftArrow)
+                if (key == ConsoleKey.LeftArrow) // 왼쪽으로 이동할 때
                 {
                     playerX = Math.Max(MAP_MIN_X , playerX - 1);
                     playerDirection = DIRECTION.LEFT;
+                    if (playerX == INITIAL_WALL_X && playerY == INITIAL_WALL_Y)
+                    {
+                        playerX = INITIAL_WALL_X + 1;
+                    }
                 }
 
-                if (key == ConsoleKey.DownArrow)
+                if (key == ConsoleKey.DownArrow) // 아래로 이동할 때
                 {
                     playerY = Math.Min(playerY + 1, MAP_MAX_Y);
                     playerDirection = DIRECTION.DOWN;
+                    if (playerX == INITIAL_WALL_X && playerY == INITIAL_WALL_Y)
+                    {
+                        playerY = INITIAL_WALL_Y - 1;
+                    }
                 }
 
-                if (key == ConsoleKey.UpArrow)
+                if (key == ConsoleKey.UpArrow) // 위로 이동할 때
                 {
                     playerY = Math.Max(MAP_MIN_Y, playerY - 1);
                     playerDirection = DIRECTION.UP;
+                    if (playerX == INITIAL_WALL_X && playerY == INITIAL_WALL_Y)
+                    {
+                        playerY = INITIAL_WALL_Y + 1;
+                    }
                 }
 
+                
 
 
 
+                    // 플레이어가 이동한 좌표를 다 구한 후!
 
-                // 플레이어가 이동한 좌표를 다 구한 후!
-
-                if (playerX == boxX && playerY == boxY) // 이동하고보니 박스가 있네?
+                    if (playerX == boxX && playerY == boxY) // 이동하고보니 박스가 있네?
                 {
                     // 박스를 움직여주기
                     // 플레이어가 어디서 왔는지 모름!
@@ -148,6 +157,7 @@
                     switch (playerDirection)
                     {
                         case DIRECTION.LEFT: // 왼쪽으로 이동중일때
+                            
                             if (boxX == MAP_MIN_X) // 박스가 왼쪽 끝에 있다면?
                             {
                                 playerX = MAP_MIN_X + 1;
@@ -156,17 +166,21 @@
                             {
                                 boxX = boxX - 1;
                             }
+                            
                             break;
 
                         case DIRECTION.RIGHT: // 오른쪽으로 이동중일 때
+                            
+
                             if (boxX == MAP_MAX_X)
                             {
                                 playerX = MAP_MAX_X - 1;
                             }
-                            else
+                            else 
                             {
                                 boxX = boxX + 1;
                             }
+                          
 
                             break;
 
@@ -179,6 +193,7 @@
                             {
                                 boxY = boxY - 1;
                             }
+                            
                             break;
 
                         case DIRECTION.DOWN: // 아래로 이동중 일때
@@ -190,6 +205,7 @@
                             {
                                 boxY = boxY + 1;
                             }
+                            
                             break;
 
                         default:
@@ -202,9 +218,54 @@
                 }
 
 
+                if (playerX == INITIAL_WALL_X && playerY == INITIAL_WALL_Y)
+                {
+                    switch (playerDirection)
+                    {
+                        case DIRECTION.LEFT:
+                            playerX = INITIAL_WALL_X + 1;
+                            break;
 
+                        case DIRECTION.RIGHT:
+                            playerX = INITIAL_WALL_X - 1;
+                            break;
 
+                        case DIRECTION.UP:
+                            playerY = INITIAL_WALL_Y + 1;
+                            break;
 
+                        case DIRECTION.DOWN:
+                            playerY = INITIAL_WALL_Y - 1;
+                            break;
+
+                    }
+                }
+
+                if (boxX == INITIAL_WALL_X && boxY == INITIAL_WALL_Y)
+                {
+                    switch(playerDirection)
+                    {
+                        case DIRECTION.LEFT:
+                            boxX = INITIAL_WALL_X + 1;
+                            playerX = boxX + 1;
+                            break;
+
+                        case DIRECTION.RIGHT:
+                            boxX = INITIAL_WALL_X - 1;
+                            playerX = boxX - 1;
+                            break;
+
+                        case DIRECTION.UP:
+                            boxY = INITIAL_WALL_Y + 1;
+                            playerY = boxY + 1;
+                            break;
+
+                        case DIRECTION.DOWN:
+                            boxY = INITIAL_WALL_Y - 1;
+                            playerY = boxY - 1;
+                            break;
+                    }
+                }
 
 
 
