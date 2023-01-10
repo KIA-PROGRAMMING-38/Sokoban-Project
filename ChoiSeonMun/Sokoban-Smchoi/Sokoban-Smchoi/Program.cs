9e60f3974450c46374c2b8f7524602c6e0fa119c
choiseonmun <seonmun.choi@gmail.com>
2023-01-10 13:42:07 +0900

    프로젝트 초기 세팅

--- ChoiSeonMun/Sokoban-Smchoi/Sokoban-Smchoi/Program.cs
+++ ChoiSeonMun/Sokoban-Smchoi/Sokoban-Smchoi/Program.cs
@@ -6,7 +6,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // 초기 세팅
+        Console.ResetColor();                               // 컬러를 초기화한다.
+        Console.CursorVisible = false;                      // 커서를 숨긴다.
+        Console.Title = "My Sokoban";                       // 타이틀을 설정한다.
+        Console.BackgroundColor = ConsoleColor.DarkBlue;    // 배경색을 설정한다.
+        Console.ForegroundColor = ConsoleColor.Gray;        // 글꼴색을 설정한다.
+        Console.Clear();                                    // 콘솔 창에 출력된 내용을 모두 지운다.
     }
 }
 
