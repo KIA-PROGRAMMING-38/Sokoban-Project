8f957426edda5fce1356a61aea5985cbf7d79ef3
choiseonmun <seonmun.choi@gmail.com>
2023-01-10 14:47:08 +0900

    플레이어 이동 구현

--- ChoiSeonMun/Sokoban-Smchoi/Sokoban-Smchoi/Program.cs
+++ ChoiSeonMun/Sokoban-Smchoi/Sokoban-Smchoi/Program.cs
@@ -13,6 +13,49 @@ class Program
         Console.BackgroundColor = ConsoleColor.DarkBlue;    // 배경색을 설정한다.
         Console.ForegroundColor = ConsoleColor.Gray;        // 글꼴색을 설정한다.
         Console.Clear();                                    // 콘솔 창에 출력된 내용을 모두 지운다.
+
+        // 플레이어 위치 좌표
+        int playerX = 0;
+        int playerY = 0;
+
+        // 게임 루프
+        while (true)
+        {
+            // ======================= Render ==========================
+            // 이전 프레임을 지운다
+            Console.Clear();
+
+            // 플레이어를 그린다
+            Console.SetCursorPosition(playerX, playerY);
+            Console.Write("P");
+
+            // ======================= ProcessInput =======================
+            // 유저로부터 입력을 받는다 
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            ConsoleKey key = keyInfo.Key;   // 실제 키는 ConsoleKeyInfo에 Key에 있다 
+
+            // ======================= Update =======================
+            // 플레이어 이동 처리 
+            if (key == ConsoleKey.UpArrow)
+            {
+                playerY = (int)Math.Max(0, playerY - 1);
+            }
+
+            if (key == ConsoleKey.DownArrow)
+            {
+                playerY = (int)Math.Min(playerY + 1, 20);
+            }
+
+            if (key == ConsoleKey.LeftArrow)
+            {
+                playerX = (int)Math.Max(0, playerX - 1);    
+            }
+
+            if (key == ConsoleKey.RightArrow)
+            {
+                playerX = (int)Math.Min(playerX + 1, 15);
+            }
+        }
     }
 }
 
